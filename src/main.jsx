98248344e699fa50ba1c60156d04bb28e7572554
Jason Theodorou <jay.alfredos.labs@gmail.com>
2026-05-26 15:05:11 +0100

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import HermesMasteryCourse from "./HermesMasteryCourse.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HermesMasteryCourse />
+  </StrictMode>,
+);
